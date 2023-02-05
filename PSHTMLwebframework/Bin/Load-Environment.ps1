8808d2aedcb2ce3a5823fd9940c025d881621ab0
Martin Walther <it@martin-walther.ch>
2023-02-05 17:42:46 +0100

    Update Load-Environment.ps1

--- PSHTMLwebframework/Bin/Load-Environment.ps1
+++ PSHTMLwebframework/Bin/Load-Environment.ps1
@@ -8,7 +8,7 @@ $Root            = ((Get-Item $Current).Parent).FullName
 $HTMLRoot        = $Root
 $HTMLAssetsPath  = Join-Path -Path $HTMLRoot -ChildPath "Assets"
 $HTMLBlogPath    = Join-Path -Path $HTMLRoot -ChildPath "Blog"
-$HTMLStylePath   = Join-Path -Path $HTMLRoot -ChildPath "Style"
+$HTMLStylePath   = Join-Path -Path $HTMLAssetsPath -ChildPath "Style"
 
 $PSBinPath       = Join-Path -Path $HTMLRoot -ChildPath "Bin"
 $PSTemplatePath  = Join-Path -Path $PSBinPath -ChildPath "Templates"
