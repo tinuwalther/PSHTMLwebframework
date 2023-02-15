44962d1ea73137455d6e14d62a14f7c8146143b8
Martin Walther <it@martin-walther.ch>
2023-02-15 20:38:12 +0100

    Update

--- PSHTMLwebframework/Bin/Includes/footer.ps1
+++ PSHTMLwebframework/Bin/Includes/footer.ps1
@@ -1,14 +1,14 @@
 
 span -id "footer" -class "text-center" -Content {
 
-    a -href "#" -class "badge rounded-pill text-bg-light" -Content {
+    a -href "#" -class "badge rounded-pill text-bg-primary" -Content {
         "Top"
     }
 
-    #$PSHTMLlink    = a {"PSHTML"} -href "https://github.com/Stephanevg/PSHTML"  -Target _blank
+    $PSHTMLlink    = a {"PSHTML"} -href "https://github.com/Stephanevg/PSHTML"  -Target _blank
     " | Based on $($PSHTMLlink) | Bootstrap v5.3.0 | "
 
-    span -class "badge rounded-pill text-bg-light" -Content {
+    span -class "badge rounded-pill text-bg-primary" -Content {
         "v0.1.2"
     }
 
